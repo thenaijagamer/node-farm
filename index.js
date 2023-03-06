58d567b5465710a11a51460a0d8497a577f12da2
thenaijagamer <sabdullahialaba50@gmail.com>
2023-03-06 23:21:42 +0100

    ..

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 console.log(textIn);
+
+const textOut = `This is what we know about avocado:\n${textIn}\nCreated on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOut);
+
+console.log(fs.readFileSync("./txt/output.txt", "utf-8"));

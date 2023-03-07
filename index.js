651bbe864e24929622d0097c575818dbb3a9c473
thenaijagamer <sabdullahialaba50@gmail.com>
2023-03-08 00:11:26 +0100

    Make changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado:\n${textIn}\nCreated on ${Date.now()}`;
+// fs.appendFileSync("./txt/output.txt", textOut);
+// console.log(fs.readFileSync("./txt/output.txt", "utf-8"));
 
-console.log(textIn);
-
-const textOut = `This is what we know about avocado:\n${textIn}\nCreated on ${Date.now()}`;
-
-fs.writeFileSync("./txt/output.txt", textOut);
-
-console.log(fs.readFileSync("./txt/output.txt", "utf-8"));
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});

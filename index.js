dc09604bea1fc8fa90126a204feccfee5e82e5ae
thenaijagamer <sabdullahialaba50@gmail.com>
2023-06-22 11:34:39 +0100

    Make small changes

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 const fs = require("fs");
 const http = require("http");
-const url = require("url");
-
-//////////////////////////////////
-// File reading and writing
-
-// Blocking, synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about avocado:\n${textIn}\nCreated on ${Date.now()}`;
-// fs.appendFileSync("./txt/output.txt", textOut);
-// console.log(fs.readFileSync("./txt/output.txt", "utf-8"));
-
-// Non-blocking, asynchronous way
-
-// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data) => {
-//       err ? console.log(err.message) : console.log(data);
-//     });
-//   }
-// });
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
@@ -34,15 +11,22 @@ const objData = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // OVERVIEW PAGE
   if (pathName == "/") {
     res.end("this is the homepage");
+
+    // PRODUCT PAGE
   } else if (pathName == "/product") {
     res.end("This is the product page");
+
+    // API
   } else if (pathName == "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    // NOT FOUND
   } else {
     res.writeHead(404, {
       "content-type": "text/html",

f13a106b8aa00477658b2510de801c786b2ad40a
thenaijagamer <sabdullahialaba50@gmail.com>
2023-06-22 22:55:25 +0100

    Change the all the colors to natural farm color combination

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const fs = require("fs");
 const http = require("http");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
 const objData = JSON.parse(data);
 
@@ -12,12 +24,18 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   // OVERVIEW PAGE
-  if (pathName == "/") {
-    res.end("this is the homepage");
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(tempOverview);
 
     // PRODUCT PAGE
   } else if (pathName == "/product") {
-    res.end("This is the product page");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(tempProduct);
 
     // API
   } else if (pathName == "/api") {

e73a0fa9c3e2d90f17da9731e9d4b74fa6850526
thenaijagamer <sabdullahialaba50@gmail.com>
2023-06-22 10:07:28 +0100

    Build a very simple API

--- index.js
+++ index.js
@@ -24,19 +24,32 @@ const url = require("url");
 //   }
 // });
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const objData = JSON.parse(data);
+
 //////////////////////////////////
 // SERVER
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
   const pathName = req.url;
+
   if (pathName == "/") {
     res.end("this is the homepage");
   } else if (pathName == "/product") {
     res.end("This is the product page");
+  } else if (pathName == "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
-    res.writeHead(404);
-    res.end("Page not found");
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "You entered the wrong path",
+    });
+
+    res.end("<h1>Page not found</h1>");
   }
 });
 
